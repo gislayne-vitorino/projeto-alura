03206dac9ddce9d3017160fdbaa26200b933e5e5
gislayne-vitorino <gislaynevitorino@gmail.com>
2024-10-06 18:29:06 -0300

    feat: User must be an instructor to be the course owner

--- src/main/java/br/com/alura/ProjetoAlura/course/CourseController.java
+++ src/main/java/br/com/alura/ProjetoAlura/course/CourseController.java
@@ -1,6 +1,9 @@
 package br.com.alura.ProjetoAlura.course;
 
 import jakarta.validation.Valid;
+
+import java.util.Objects;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,30 +12,48 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.ProjetoAlura.user.Role;
+import br.com.alura.ProjetoAlura.user.User;
+import br.com.alura.ProjetoAlura.user.UserRepository;
 import br.com.alura.ProjetoAlura.util.ErrorItemDTO;
 
 @RestController
 public class CourseController {
 	
 	private final CourseRepository courseRepository;
+    private final UserRepository userRepository;
 
-	public CourseController(CourseRepository courseRepository) {
+	
+	public CourseController(CourseRepository courseRepository, UserRepository userRepository) {
 		this.courseRepository = courseRepository;
+        this.userRepository = userRepository;
+
 	}
 	
     @Transactional
     @PostMapping("/course/new")
-    public ResponseEntity createCourse(@Valid @RequestBody NewCourseDTO newCourse) {
+    public ResponseEntity createCourse(@Valid @RequestBody NewCourseDTO newCourseDTO) {
 
-    	if(courseRepository.existsByCode(newCourse.getCode())) {
+    	if(courseRepository.existsByCode(newCourseDTO.getCode())) {
     			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
     					.body(new ErrorItemDTO("code", "Código já cadastrado no sistema"));
-    		}
-    		
-    		//checar se usuário é instrutor
-    		    		
-    		Course course = newCourse.toModel();
-    		courseRepository.save(course);
+		}
+		    		
+        User user = userRepository.findByEmail(newCourseDTO.getInstructorEmail());
+
+        if(user == null) {
+        	return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body(new ErrorItemDTO("email", "Não há usuário com esse email cadastrado"));
+        }
+        
+        if(!Objects.equals(user.getRole().INSTRUCTOR, Role.INSTRUCTOR )) {
+        	return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body(new ErrorItemDTO("email", "Usuário deve ser instrutor para cadastrar um curso"));
+
+        }
+        
+		Course course = newCourseDTO.toModel();
+		courseRepository.save(course);
     		
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
