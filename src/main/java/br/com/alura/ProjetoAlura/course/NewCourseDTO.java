375d880243fe54b753b68fa52281d1231d6a9a93
gislayne-vitorino <gislaynevitorino@gmail.com>
2024-10-06 18:39:27 -0300

    feat: Add regex to only allow code to be added with letters or hyphen separting the words

--- src/main/java/br/com/alura/ProjetoAlura/course/NewCourseDTO.java
+++ src/main/java/br/com/alura/ProjetoAlura/course/NewCourseDTO.java
@@ -3,6 +3,8 @@ package br.com.alura.ProjetoAlura.course;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 import org.hibernate.validator.constraints.Length;
 
 public class NewCourseDTO {
@@ -14,6 +16,7 @@ public class NewCourseDTO {
     @NotNull
     @NotBlank
     @Length(min = 4, max = 10)
+    @Pattern(regexp = "^[a-zA-Z]+(-[a-zA-Z]+)*$", message = "Código só pode conter letras e hífens para separar")
     private String code;
 
     private String description;
